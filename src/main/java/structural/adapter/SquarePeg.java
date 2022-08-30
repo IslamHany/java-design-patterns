002a6e47f954fd5d8b485013d18bb60cd62c13b4
islamHany <islamhany2268@gmail.com>
2022-08-30 22:02:06 +0200

    adapter

---
+++ src/main/java/structural/adapter/SquarePeg.java
@@ -0,0 +1,22 @@
+package structural.adapter;
+
+/**
+ * SquarePegs are not compatible with RoundHoles (they were implemented by
+ * previous development team). But we have to integrate them into our program.
+ */
+
+public class SquarePeg {
+    private double width;
+
+    public SquarePeg(double width) {
+        this.width = width;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getSquare(){
+        return this.width * this.width;
+    }
+}
