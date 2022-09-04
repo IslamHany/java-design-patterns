e35ee06e5616035d7c15e986144577c7d14de424
islamHany <islamhany2268@gmail.com>
2022-09-04 14:49:14 +0200

    facade

---
+++ src/main/java/structural/facade/MPEG4CompressionCodec.java
@@ -0,0 +1,5 @@
+package structural.facade;
+
+public class MPEG4CompressionCodec implements Codec {
+    private final String type = "mp4";
+}
---
+++ src/main/java/structural/facade/OggCompressionCodec.java
@@ -0,0 +1,5 @@
+package structural.facade;
+
+public class OggCompressionCodec implements Codec {
+    private final String type = "ogg";
+}
